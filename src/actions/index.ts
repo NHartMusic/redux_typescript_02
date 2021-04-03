7251b20101d5f4b70443585976199b4b1a9075ef
Nick Hart <nhartmusic@gmail.com>
2021-04-03 18:02:31 +0200

    setting up action enums and generic dispatch functions

---
+++ src/actions/index.ts
@@ -0,0 +1,27 @@
+import axios from 'axios'
+import { Dispatch } from 'redux'
+import { ActionTypes } from './types'
+
+interface ITodo { 
+    id: number
+    title: string
+    completed: boolean
+}
+
+interface IFetchTodosAction {
+    type: ActionTypes.fetchTodos
+    payload: ITodo[]
+}
+
+const url = 'https://jsonplaceholder.typicode.com/todos'
+
+export const fetchTodos = () => {
+    return async (dispatch: Dispatch) => {
+        const response = await axios.get<ITodo[]>(url)
+
+        dispatch<IFetchTodosAction> ({
+            type: ActionTypes.fetchTodos, 
+            payload: response.data
+        })
+    }
+}
