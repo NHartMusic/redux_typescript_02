7e35b592a9f774483f3e8993dd85fdcdd503bd9c
Nick Hart <nhartmusic@gmail.com>
2021-04-04 17:11:55 +0200

    rendering list to screen

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,41 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { ITodo, fetchTodos } from '../actions'
+import { StoreState } from '../reducers'
 
-export class App extends React.Component {
+interface AppProps {
+    todos: ITodo[]
+    fetchTodos(): any
+}
+
+class _App extends React.Component<AppProps> {
+    onButtonClick = (): void => {
+        this.props.fetchTodos()
+    }
+
+    renderList(): JSX.Element[] {
+        return this.props.todos.map(( todo: ITodo) => {
+            return <div key={todo.id}>{todo.title}</div>
+        })
+    }
+
     render() {
-        return <div>hi</div>
+        return (
+            <div>
+                <button onClick={this.onButtonClick}>
+                    Fetch
+                </button>
+                {this.renderList()}
+            </div>
+        )
     }
 }
+
+const mapStateToProps = ({ todos }: StoreState): { todos: ITodo[] } => {
+    return { todos }
+}
+
+export const App = connect(
+    mapStateToProps,
+    { fetchTodos }
+)(_App)
